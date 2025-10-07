cba15b8c88d1d15d63b05f74b25685e04cb7ed2a
L30Zh <LEoz47@nycstudents.net>
2025-10-07 09:29:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+""" english = 't' or 'T'
+french = 's' or 'S'
+t_count = 0
+s_count = 0
+clause = input("What you wanna say") 
+for char in clause:
+        if char == english:
+            t_count += 1
+        elif char == french:
+            s_count += 1
+if s_count < t_count:
+            print("English")
+else:
+    print ("French") """
 
 
-language = input("gng whatchu wanna say")
-bob = list(input)
-    print bob
+
+empty = "."
+Oc = "C"
+
+input ("first day?")
+            
+
+
     
-
-
-
